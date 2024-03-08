f5c845ed8558186956b14e674cc3fe4facecaaff
Levin E P <levin.padmarajan@qburst.com>
2024-03-08 13:35:08 +0530

    Controller and model

---
+++ userauth/src/main/java/com/ecommerce/userauth/controllers/UserController.java
@@ -0,0 +1,9 @@
+package com.ecommerce.userauth.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+}
